07eb749b3ac2568995a456640dc44d9caa822c74
codecrafterpixel-debug <codecrafterpixel@gmail.com>
2026-09-24 15:30:57 +0530

    Update CORS headers in delete.js

--- api/delete.js
+++ api/delete.js
@@ -2,10 +2,14 @@ import { del } from '@vercel/blob';
 import { getDb, initDb } from './db.js';
 
 export default async function handler(req, res) {
-  // CORS
+  // CORS Headers
+  res.setHeader('Access-Control-Allow-Credentials', 'true');
   res.setHeader('Access-Control-Allow-Origin', '*');
-  res.setHeader('Access-Control-Allow-Methods', 'DELETE, POST, OPTIONS');
-  res.setHeader('Access-Control-Allow-Headers', 'Content-Type');
+  res.setHeader('Access-Control-Allow-Methods', 'GET,OPTIONS,PATCH,DELETE,POST,PUT');
+  res.setHeader(
+    'Access-Control-Allow-Headers',
+    'X-CSRF-Token, X-Requested-With, Accept, Accept-Version, Content-Length, Content-MD5, Content-Type, Date, X-Api-Version'
+  );
 
   if (req.method === 'OPTIONS') {
     return res.status(200).end();
